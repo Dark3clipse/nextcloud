fc89aaf7505cf92d6cf195bd8ff8c5800f6b2d8c
Sophia Hadash <sophiahadash@gmail.com>
2020-10-11 21:34:23 +0200

    fix variables

--- run.sh
+++ run.sh
@@ -7,7 +7,12 @@ fi
 
 # test availability of environment variables
 if [ -z "$MYSQL_PASSWORD" ]; then echo "Environment variable MYSQL_PASSWORD is unset or empty." && exit 1; fi
+if [ -z "$NEXTCLOUD_PATH" ]; then echo "Environment variable NEXTCLOUD_PATH is unset or empty." && exit 1; fi
 
+# define some derivative variables
+export MYSQL_ROOT_PASSWORD=$MYSQL_PASSWORD
+export MYSQL_DATABASE=nextcloud
+export MYSQL_USER=nextcloud
 
 # deploy
-docker-compose -f nextcloud.yml -p nextcloud up -d
+docker-compose --env-file ./.env -f nextcloud.yml -p nextcloud up -d
