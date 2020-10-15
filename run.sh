8736f69d1b8a0ae139a62a756733c718af871be7
Sophia Hadash <sophiahadash@gmail.com>
2020-10-15 18:18:35 +0200

    secure secrets

--- run.sh
+++ run.sh
@@ -10,6 +10,10 @@ if [ -z "$MYSQL_PASSWORD" ]; then echo "Environment variable MYSQL_PASSWORD is u
 if [ -z "$NEXTCLOUD_PATH" ]; then echo "Environment variable NEXTCLOUD_PATH is unset or empty." && exit 1; fi
 if [ -z "$NEXTCLOUD_PATH" ]; then echo "Environment variable BACKUP_PATH is unset or empty." && exit 1; fi
 if [ -z "$SYNAPSE_POSTGRESS_PASSWORD" ]; then echo "Environment variable SYNAPSE_POSTGRESS_PASSWORD is unset or empty." && exit 1; fi
+if [ -z "$SYNAPSE_KEY_REG" ]; then echo "Environment variable SYNAPSE_KEY_REG is unset or empty." && exit 1; fi
+if [ -z "$SYNAPSE_KEY_MACARON" ]; then echo "Environment variable SYNAPSE_KEY_MACARON is unset or empty." && exit 1; fi
+if [ -z "$SYNAPSE_KEY_FORM" ]; then echo "Environment variable SYNAPSE_KEY_FORM is unset or empty." && exit 1; fi
+if [ -z "$SYNAPSE_KEY_SIGNING" ]; then echo "Environment variable SYNAPSE_KEY_SIGNING is unset or empty." && exit 1; fi
 
 # define some derivative variables
 export MYSQL_ROOT_PASSWORD=$MYSQL_PASSWORD
@@ -21,11 +25,18 @@ git clone https://github.com/helmuthb/rsnapshot-docker
 
 
 # generate synapse default config
-docker run -it --rm \
-    --mount type=bind,src=$NEXTCLOUD_PATH/synapse,target=/data \
-    -e SYNAPSE_SERVER_NAME=synapse.sophiahadash.nl \
-    -e SYNAPSE_REPORT_STATS=yes \
-    matrixdotorg/synapse:3af9bb7198c64c6e6001b96cbfadc0bc2123d318-multiarch generate
+#docker run -it --rm \
+#    --mount type=bind,src=$NEXTCLOUD_PATH/synapse,target=/data \
+#    -e SYNAPSE_SERVER_NAME=synapse.sophiahadash.nl \
+#    -e SYNAPSE_REPORT_STATS=yes \
+#    matrixdotorg/synapse:3af9bb7198c64c6e6001b96cbfadc0bc2123d318-multiarch generate
+sed -i "s#__SYNAPSE_POSTGRESS_PASSWORD__#$SYNAPSE_POSTGRESS_PASSWORD#g" ./synapse/homeserver.yaml
+sed -i "s#__SYNAPSE_KEY_REG__#$SYNAPSE_KEY_REG#g" ./synapse/homeserver.yaml
+sed -i "s#__SYNAPSE_KEY_MACARON__#$SYNAPSE_KEY_MACARON#g" ./synapse/homeserver.yaml
+sed -i "s#__SYNAPSE_KEY_FORM__#$SYNAPSE_KEY_FORM#g" ./synapse/homeserver.yaml
+sed -i "s#__SYNAPSE_KEY_SIGNING__#$SYNAPSE_KEY_SIGNING#g" ./synapse/synapse.sophiahadash.nl.signing.key
+sed -i "s#__SYNAPSE_POSTGRESS_PASSWORD__#$SYNAPSE_POSTGRESS_PASSWORD#g" ./synapse/whatsapp/config.yaml
+
 
 # deploy
 docker-compose --env-file ./.env -f nextcloud.yml -p letsencrypt up -d
