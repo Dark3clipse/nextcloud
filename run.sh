fca5526ef205bd1367cc49f423bb9b921dad3d3f
Sophia Hadash <sophiahadash@gmail.com>
2020-12-14 15:35:16 +0100

    add whatsapp bridge

--- run.sh
+++ run.sh
@@ -10,6 +10,9 @@ if [ -z "$MYSQL_PASSWORD" ]; then echo "Environment variable MYSQL_PASSWORD is u
 if [ -z "$NEXTCLOUD_PATH" ]; then echo "Environment variable NEXTCLOUD_PATH is unset or empty." && exit 1; fi
 if [ -z "$RECAPTCHA_PUBLIC" ]; then echo "Environment variable RECAPTCHA_PUBLIC is unset or empty." && exit 1; fi
 if [ -z "$RECAPTCHA_PRIVATE" ]; then echo "Environment variable RECAPTCHA_PRIVATE is unset or empty." && exit 1; fi
+if [ -z "$WHATSAPP_AS" ]; then echo "Environment variable WHATSAPP_AS is unset or empty." && exit 1; fi
+if [ -z "$WHATSAPP_HS" ]; then echo "Environment variable WHATSAPP_HS is unset or empty." && exit 1; fi
+if [ -z "$WHATSAPP_C" ]; then echo "Environment variable WHATSAPP_C is unset or empty." && exit 1; fi
 
 # define some derivative variables
 export MYSQL_ROOT_PASSWORD=$MYSQL_PASSWORD
@@ -21,6 +24,12 @@ export POSTGRES_PASSWORD=$MYSQL_PASSWORD
 sed -i "s/__POSTGRESQL_PASSWORD__/$MYSQL_PASSWORD/g" ./synapse/homeserver.yaml
 sed -i "s/__RECAPTCHA_PRIVATE__/$RECAPTCHA_PRIVATE/g" ./synapse/homeserver.yaml
 sed -i "s/__RECAPTCHA_PUBLIC__/$RECAPTCHA_PUBLIC/g" ./synapse/homeserver.yaml
+sed -i "s/__POSTGRESQL_PASSWORD__/$MYSQL_PASSWORD/g" ./synapse/whatsapp/config.yaml
+sed -i "s/__WHATSAPP_AS__/$WHATSAPP_AS/g" ./synapse/whatsapp/config.yaml
+sed -i "s/__WHATSAPP_HS__/$WHATSAPP_HS/g" ./synapse/whatsapp/config.yaml
+sed -i "s/__WHATSAPP_AS__/$WHATSAPP_AS/g" ./synapse/appservices/whatsapp.yaml
+sed -i "s/__WHATSAPP_HS__/$WHATSAPP_HS/g" ./synapse/appservices/whatsapp.yaml
+sed -i "s/__WHATSAPP_C__/$WHATSAPP_C/g" ./synapse/appservices/whatsapp.yaml
 
 # clone rsnapshot-docker
 if [ ! -d ./rsnapshot-docker ]; then
